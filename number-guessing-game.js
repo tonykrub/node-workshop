4955d66e2fdbda127141901a311ca0719dae47f6
Tony <tonykrub@hotmail.com>
2016-07-12 18:25:29 +0000

    Workshop 4: Node Workshop - Number Guessing Game!

---
+++ number-guessing-game.js
@@ -0,0 +1,8 @@
+/* 5)Number guessing game!
+Create a file called number-guessing-game.js.
+In this file, re-write your number guessing game (from the basic javascript workshop) for the 
+command line!
+Instead of using prompt and alert, you will have to use capabilities from NodeJS and any 
+external module. HINT: there is an npm library called prompt that can help you with that :)
+Save/commit/push
+*/
