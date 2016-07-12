774627270c4b51ef211a37ab31349efa847e82db
Tony <tonykrub@hotmail.com>
2016-07-12 20:08:54 +0000

    Workshop4: Node Workshop - Number Guessing Game

--- number-guessing-game.js
+++ number-guessing-game.js
@@ -6,3 +6,31 @@ Instead of using prompt and alert, you will have to use capabilities from NodeJS
 external module. HINT: there is an npm library called prompt that can help you with that :)
 Save/commit/push
 */
+var prompt = require("prompt");
+var randomNumber = Math.floor(Math.random() * 100);
+var counter = 3;
+function guessingNumber(number) {
+    prompt.get('Hi..Please guess a number', function(err, result) {
+        if (err) {
+            console.log("There is an error.");
+        }
+        else if (result.guess === randomNumber) {     // why is it result.guess?
+            console.log("Good job! You are right!");
+        }
+        else if (result.guess > randomNumber) {
+            console.log("Your number is too high!");
+        }
+        else {
+            console.log("Your number is too low!");
+        }
+        if (counter > 0) {
+            counter --;
+            guessingNumber();
+            }
+        else {
+            console.log("The guess is over!");
+        }
+    });
+}
+guessingNumber();
+        
